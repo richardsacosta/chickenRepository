e0740814ad907c6467309d2a3205f6945922670d
Mercado <rmercado@urbanscience.com>
2024-03-12 15:20:41 -0500

    prepare base model completd

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ __version__ = "0.0.0"
 REPO_NAME = "chickenRepository"
 AUTHOR_USER_NAME = "richardsacosta"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "richardsacosta@gmail.com"
 
 
 setuptools.setup(
